d740dc7fbc5288b0db3665e05072d8082ba50abf
ozern <ozenov07@bk.ru>
2025-04-27 12:24:40 +0300

    refactor(other.py): Форматирование кода, исправление ошибок, изменений названий функций.
    1) Улучшено форматирование для единообразия кода.
    2) Прописано более информативное логирование.

--- app/src/handlers/user/private/other.py
+++ app/src/handlers/user/private/other.py
@@ -1,4 +1,5 @@
 """Helper functions for handlers"""
+
 import logging
 import json
 
@@ -12,22 +13,24 @@ from pydantic import ValidationError
 from ....utils.schemas import AccountSchema
 from ....parsers.info import AccountInfo, get_info_about_account
 from ....utils.formatted_output import format_account_info, format_player_info_in_match
-from ....utils.keyboards import account_buttons, paginated_buttons, special_button_for_account
+from ....utils.keyboards import (
+    account_buttons,
+    paginated_buttons,
+    special_button_for_account,
+)
 
 
 async def process_account_id(
-            account_id: str,
-            chat_id: int,
-            state: FSMContext,
-            bot: Bot,
-            locale: TranslatorRunner):
+    account_id: str, chat_id: int, state: FSMContext, bot: Bot, locale: TranslatorRunner
+):
 
     try:
         account_id = AccountSchema(account_id=int(account_id))
         await state.update_data(account_id=account_id.account_id)
         await bot.send_message(
             chat_id=chat_id,
-            text=locale.detected_account(account_id=account_id.account_id))
+            text=locale.detected_account(account_id=account_id.account_id),
+        )
 
         query: AccountInfo = await get_info_about_account(account_id.account_id, locale)
         if query is None:
@@ -36,8 +39,8 @@ async def process_account_id(
 
         result = format_account_info(query, locale)
 
-        if result.count('None') > 0:
-            result += f'\n\n{locale.probably_account_hidden()}'
+        if result.count("None") > 0:
+            result += f"\n\n{locale.probably_account_hidden()}"
 
         await bot.send_photo(
             chat_id=chat_id,
@@ -45,38 +48,38 @@ async def process_account_id(
             caption=result,
             reply_markup=await special_button_for_account(query.profile_url, locale),
         )
-        logging.info(f'Информация для пользователя "{account_id.account_id}" успешно отправлена')
+        logging.info(
+            f'Информация для пользователя "{account_id.account_id}" успешно отправлена'
+        )
     except ValidationError as e:
         await bot.send_message(chat_id=chat_id, text=locale.error_validation())
-        logging.exception(f"Ошибка ValidationError: {e}")
+        logging.exception(f"Ошибка ValidationError в process_account_id: {e}", exc_info=True)
         return
 
     except ValueError as e:
         await bot.send_message(chat_id=chat_id, text=f"{locale.incorrect_input()} {e}")
-        logging.exception(f"Ошибка ValueError: {e}")
+        logging.exception(f"Ошибка ValueError в process_account_id: {e}", exc_info=True)
         return
 
     except TelegramBadRequest as e:
         await bot.send_message(chat_id=chat_id, text=locale.error_sending())
-        logging.exception(f"Ошибка TelegramBadRequest: {e}")
+        logging.exception(f"Ошибка TelegramBadRequest в process_account_id: {e}", exc_info=True)
         return
 
     except Exception as e:
         await bot.send_message(chat_id=chat_id, text=locale.unexpected_error())
-        logging.exception(f"Ошибка Exception: {e}")
+        logging.exception(f"Ошибка в process_account_id: {e}", exc_info=True)
         return
 
 
-async def show_carousel(callback: CallbackQuery,
-                        state: FSMContext,
-                        page: int,
-                        locale: TranslatorRunner):
+async def show_carousel(
+    callback: CallbackQuery, state: FSMContext, page: int, locale: TranslatorRunner
+):
     data = await state.get_data()
-    accounts = data.get('accounts_data')
+    accounts = data.get("accounts_data")
     if accounts is None:
         await callback.answer("Нет данных о аккаунтах.")
         return
-
 
     keyboard = await account_buttons(page, accounts, locale)
 
@@ -84,16 +87,14 @@ async def show_carousel(callback: CallbackQuery,
     await callback.message.edit_reply_markup(reply_markup=keyboard)
 
 
-async def show_page(callback: CallbackQuery,
-                    state: FSMContext,
-                    page: int,
-                    locale: TranslatorRunner):
+async def show_page(
+    callback: CallbackQuery, state: FSMContext, page: int, locale: TranslatorRunner
+):
     """Функция для отображения страницы с пагинацией."""
 
     data = await state.get_data()
-    players_data = data.get('players_data')
-    number_pages = data.get('number_pages_for_match', 1)
-
+    players_data = data.get("players_data")
+    number_pages = data.get("number_pages_for_match", 1)
 
     if players_data is None:
         logging.error("Данные о игроках отсутствуют.")
@@ -103,7 +104,7 @@ async def show_page(callback: CallbackQuery,
     try:
         text = format_player_info_in_match(json.loads(players_data), page, locale)
     except Exception as e:
-        logging.exception(f"Произошла ошибка при отображении страницы: {e}")
+        logging.exception(f"Произошла ошибка при отображении страницы в show_page: {e}", exc_info=True)
         await callback.answer(locale.error_sending())
         return
 
@@ -112,16 +113,13 @@ async def show_page(callback: CallbackQuery,
         await callback.message.edit_text(text, reply_markup=keyboard)
 
     except TelegramBadRequest as e:
-        logging.warning(f"TelegramBadRequest при редактировании сообщения: {e}")
+        logging.warning(f"TelegramBadRequest при редактировании сообщения в show_page: {e}", exc_info=True)
         await callback.message.answer(locale.unexpected_error(), reply_markup=keyboard)
         return
 
     except Exception as e:
-        logging.exception(f"Произошла ошибка при отображении страницы: {e}")
+        logging.exception(f"Произошла ошибка при отображении страницы в show_page: {e}", exc_info=True)
         await callback.answer(locale.error_sending())
         return
 
     await callback.answer()
-
-
-
