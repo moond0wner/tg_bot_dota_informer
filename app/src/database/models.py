97a08406a9de7eb2a575e54dd5e3abece56f8083
ozern <ozenov07@bk.ru>
2025-04-27 13:27:21 +0300

    feat(models.py): Создана новая ячейка в таблице БД, для подсчета кол-во запросов.

--- app/src/database/models.py
+++ app/src/database/models.py
@@ -1,10 +1,14 @@
-from sqlalchemy import DateTime, String, BigInteger
+from sqlalchemy import DateTime, String, BigInteger, func
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    updated: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+    created: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+    updated: Mapped[DateTime] = mapped_column(
+        DateTime, default=func.now(), onupdate=func.now()
+    )
+
 
 class User(Base):
     __tablename__ = "users"
@@ -12,4 +16,4 @@ class User(Base):
     tg_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
     name: Mapped[str] = mapped_column(String(255))
     language: Mapped[str] = mapped_column(String(2), nullable=True)
-    number_of_requests: Mapped[int] = mapped_column()
+    number_of_requests: Mapped[int] = mapped_column(default=0, nullable=False)
